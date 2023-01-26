2cc0f75e109622b5221f6f4b27dbea6bc74366ba
izzywest <misseasybella@gmail.com>
2023-01-26 18:21:39 +0100

    #5 implement calculation for the area of a circle

--- src/main/java/at/fhtw/slm_exam_design/Controller/CalcController.java
+++ src/main/java/at/fhtw/slm_exam_design/Controller/CalcController.java
@@ -1,4 +1,15 @@
 package at.fhtw.slm_exam_design.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalcController {
+
+    @GetMapping("/circle/{num}")
+    double calcCircleArea(@PathVariable double num) {
+        return (num * num) * 3.14159265359;
+    }
+
 }
