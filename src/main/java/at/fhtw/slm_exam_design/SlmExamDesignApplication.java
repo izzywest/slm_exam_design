dbcdcc6b84bea502e76ed32758e8a27bb622fc37
i z z y <misseasybella@gmail.com>
2023-01-26 18:03:08 +0100

    #2 Generate spring files, spring web dependencies
    Generated on start.spring.io and uploaded to the REST branch on the repository.

---
+++ src/main/java/at/fhtw/slm_exam_design/SlmExamDesignApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.slm_exam_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmExamDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmExamDesignApplication.class, args);
+	}
+
+}
